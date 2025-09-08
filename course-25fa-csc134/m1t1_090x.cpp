c9caab8cecb95d960ebd96e04d3ed1aa33ee0fab
Melissa Hernandez <hernandm6224@student.faytechcc.edu>
2025-09-08 02:30:32 +0000

    Finish m1t1

---
+++ course-25fa-csc134/m1t1_090x.cpp
@@ -0,0 +1,27 @@
+/*
+CSC 134
+M1T1 for the online sections
+hernandezm
+9/7/2025
+Program will greet the user.
+
+
+
+
+*/
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    //input: start the program
+    //processing: none
+    // output: 0 (no errors)
+    // special FX: Says "Hello" to the user.
+
+    //Let's say hello
+    cout << "Hello, user!" << endl; // "endline" not "end1"
+
+    return 0; //no errors
+}
