d7bdf533ff7bda6f49feab3e025a9a51f1ad6b4e
Melissa Hernandez <hernandm6224@student.faytechcc.edu>
2025-09-14 01:27:05 +0000

    m1lab

---
+++ course-25fa-csc134/m1lab_090x.cpp
@@ -0,0 +1,29 @@
+// CSC 134
+// M1LAB
+// hernandezm
+// 9/13/2025
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //cout << "Hello World\n";
+    string name = "Melissa Hernandez";
+    int appes = 100;
+    double pricePerApple = 0.25;
+
+    cout << "Welcome to " <<name;
+    cout << "'s apple farm" << endl;
+
+    cout << "We have " << apples << " apples
+    for sale". <<endl;
+
+    cout << "Price is $" << pricePerApple << "
+    each." <<endl;
+
+    // now calculate total price 
+    double totalPrice = (double) apples *
+    pricePerApple;
+    cout << total 
+
+}
