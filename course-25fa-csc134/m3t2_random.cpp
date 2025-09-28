1237cf241df771faacefe74556ccc9d64310cd2e
Melissa Hernandez <hernandm6224@student.faytechcc.edu>
2025-09-28 20:53:59 +0000

    M3 RNG example

---
+++ course-25fa-csc134/m3t2_random.cpp
@@ -0,0 +1,16 @@
+// CSC 134
+// Start with dice rolls
+
+
+
+
+#include <iostream>
+// for random
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+int main() {
+    cout << "Hello!" << endl;
+    return 0;
+}
